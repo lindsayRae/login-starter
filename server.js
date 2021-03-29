bcb36a417a6abd49c75a936f724a4652d8a23eec
Lindsay <lbarnett712@gmail.com>
2021-03-29 06:44:40 -0400

    initial commit for express

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 1234;
+
+app.get('/', (req, res) => {
+  res.json([
+    {
+      id: 1,
+      title: 'Hello World',
+    },
+  ]);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
