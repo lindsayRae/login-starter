654bc61c51cc09693c3920b77b27d861f3915730
Lindsay <lbarnett712@gmail.com>
2021-04-03 09:46:12 -0400

    added mongoose connection and user model

---
+++ models/user.model.js
@@ -0,0 +1,80 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 1,
+      maxlength: 99,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 1,
+      maxlength: 99,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      minlength: 5,
+      maxlength: 255,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 8,
+      maxlength: 1024,
+    },
+    GUID: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// add method to user object
+// cannot user arrow function here since it does not have their own 'this'
+// if you want to create a method that is part of an object, do not use arrow function
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ _id: this._id }, config.get('jwtPrivateKey'));
+  return token;
+};
+
+const User = mongoose.model('User', userSchema);
+
+function validateUser(user) {
+  const schema = Joi.object({
+    firstName: Joi.string().min(1).max(99).required(),
+    lastName: Joi.string().min(1).max(99).required(),
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(8).max(1024).required(),
+  });
+
+  const { error, value } = schema.validate(user);
+  return { error, value };
+}
+
+function validateLogin(login) {
+  const schema = Joi.object({
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(8).max(1024).required(),
+  });
+  const { error, value } = schema.validate(login);
+  return { error, value };
+}
+
+exports.User = User;
+exports.validateUser = validateUser;
+exports.validateLogin = validateLogin;
--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 1234;
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MogoDB database connection established successfully');
+});
 
 const postsRouter = require('./routes/posts');
+const usersRouter = require('./routes/users');
+
+app.use(cors());
+app.use(express.json());
 
 app.use('/api/posts', postsRouter);
+app.use('/api/users', usersRouter);
 
 if (process.env.NODE_ENV === 'production') {
+  console.log('in production');
   // serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
 
